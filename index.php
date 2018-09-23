a0ab94f66260207cb883d7c00b0c56d9b671a2f6
Irshad Khan <khanirshad2019@gmail.com>
2018-09-23 19:46:08 +0530

    made header footer of index.php

--- index.php
+++ index.php
@@ -1,35 +1,6 @@
-<?php 
-   include("includes/config.php");//as sessionstart in config..
-//if session is set then only display page
-   if(isset($_SESSION['userLoggedIn'])){
-   	$userLoggedIn= $_SESSION['userLoggedIn'];//store username  in userloggedin
-       }
-   
-   	else{
-   		//if session not set then
-   		header("Location:register.php");
-   	}
-   
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>My App</title>
-  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
-</head>
-<body>
-<div id="mainContainer">
-
-   <div id="topContainer">
-      <?php include("includes/navBarContainer.php");?>
+<?php include "includes/header.php" ?>
 
 
-   </div>
-   <?php include("includes/nowPlayingBar.php");?>
-   
-</div>
 
-
-</body>
-</html>
+<?php include "includes/footer.php" ?>
