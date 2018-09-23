9e7b6839fc1cab044d048d118c7dda66a48aed01
Irshad Khan <khanirshad2019@gmail.com>
2018-09-23 20:13:56 +0530

    data outputted from db

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 
 <?php include "includes/header.php" ?>
+<div class="gridViewContainer">
+    <?php 
+       $albumQuery=mysqli_query($con,"SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+          
+      while ($row=mysqli_fetch_array($albumQuery)){
+         echo "<div class='gridViewItem'>
+
+             <img src='".$row['artworkPath']. "'>
+                <div class='gridViewInfo'>"
+                .$row['title'].
+                "</div>
+         </div>";
+     } 
 
 
 
+ ?>
+ </div>
 <?php include "includes/footer.php" ?>
